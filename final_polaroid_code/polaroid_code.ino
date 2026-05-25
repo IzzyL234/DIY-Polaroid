c3432a8a2273c52993f5a143a8cd4c6dc4ea97b2
IzzyL234 <1049413@lwsd.org>
2026-05-25 15:50:58 -0700

    Update polaroid_code.ino

--- final_polaroid_code/polaroid_code.ino
+++ final_polaroid_code/polaroid_code.ino
@@ -1,6 +1,53 @@
+#include "esp_camera.h"
+
+#define CAMERA_MODEL_WROVER_KIT
+#include "camera_pins.h"
+
 HardwareSerial printerSerial(1);
 const int buttonPin = 7;
 void setup() {
+  Serial.begin(115200);
+
+  camera_config_t config;
+
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  
+
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+
+
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_GRAYSCALE;
+  config.frame_size = FRAMESIZE_CIF;
+  config.fb_count = 1;
+  config.grab_mode = CAMERA_GRAB_LATEST;
+
+  esp_err_t err = esp_camera_init(&config);
+
+  if (err != ESP_OK) {
+    Serial.println("Camera init failed");
+    return;
+  }
+
   pinMode(buttonPin, INPUT_PULLUP);
 
   printerSerial.begin(9600, SERIAL_8N1, -1, 5);
@@ -165,4 +212,3 @@ void printImage(uint8_t* bitmap, int width, int height) {
   printerSerial.write(0x0A);
 }
 
-}
