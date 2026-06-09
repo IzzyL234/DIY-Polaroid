4d8db0e9b53c3e85c54cca1cb26eb450f4d2af68
IzzyL234 <1049413@lwsd.org>
2026-06-09 00:33:07 -0700

    Refactor button handling and sleep mode logic

--- final_polaroid_code/polaroid_code.ino
+++ final_polaroid_code/polaroid_code.ino
@@ -1,12 +1,86 @@
 #include "esp_camera.h"
+#include "esp_sleep.h"
 
 #define CAMERA_MODEL_WROVER_KIT
 #include "camera_pins.h"
 
 HardwareSerial printerSerial(1);
-const int buttonPin = 7;
+
+// IMPORTANT: use GPIO13, not GPIO7.
+// GPIO13 supports deep sleep wake.
+const int buttonPin = 13;
+
+const unsigned long HOLD_TIME = 2000; // 2 seconds
+
+bool buttonWasDown = false;
+unsigned long buttonDownStart = 0;
+bool longPressHandled = false;
+
+void enterOffMode() {
+  Serial.println("Turning off / entering deep sleep...");
+  delay(300);
+
+  // Wake up when buttonPin goes LOW
+  esp_sleep_enable_ext0_wakeup((gpio_num_t)buttonPin, 0);
+
+  esp_deep_sleep_start();
+}
+
+bool buttonHeldForTwoSeconds() {
+  unsigned long startTime = millis();
+
+  while (digitalRead(buttonPin) == LOW) {
+    if (millis() - startTime >= HOLD_TIME) {
+      return true;
+    }
+    delay(10);
+  }
+
+  return false;
+}
+
+void waitForButtonRelease() {
+  while (digitalRead(buttonPin) == LOW) {
+    delay(10);
+  }
+  delay(200); // debounce
+}
+
 void setup() {
   Serial.begin(115200);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  /*
+    Battery always connected behavior:
+
+    - When battery is first connected, the ESP32 starts.
+    - If the button is NOT being held for 2 seconds, it goes to sleep.
+    - To turn on, hold the shutter button for 2 seconds.
+  */
+
+  if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0) {
+    Serial.println("Woke up from button press.");
+
+    if (!buttonHeldForTwoSeconds()) {
+      Serial.println("Button was not held long enough. Going back to sleep.");
+      enterOffMode();
+    }
+
+    Serial.println("Held for 2 seconds. Turning on.");
+    waitForButtonRelease();
+  } 
+  else {
+    Serial.println("Cold boot / battery connected.");
+
+    if (!buttonHeldForTwoSeconds()) {
+      Serial.println("Button was not held for startup. Going to sleep.");
+      enterOffMode();
+    }
+
+    Serial.println("Startup hold detected. Turning on.");
+    waitForButtonRelease();
+  }
 
   camera_config_t config;
 
@@ -21,7 +95,6 @@ void setup() {
   config.pin_d5 = Y7_GPIO_NUM;
   config.pin_d6 = Y8_GPIO_NUM;
   config.pin_d7 = Y9_GPIO_NUM;
-  
 
   config.pin_xclk = XCLK_GPIO_NUM;
   config.pin_pclk = PCLK_GPIO_NUM;
@@ -33,7 +106,6 @@ void setup() {
 
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-
 
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_GRAYSCALE;
@@ -48,34 +120,71 @@ void setup() {
     return;
   }
 
-  pinMode(buttonPin, INPUT_PULLUP);
-
   printerSerial.begin(9600, SERIAL_8N1, -1, 5);
   delay(3000);
+
+  Serial.println("Device is on. Short press = capture. Hold 2 sec = off.");
 }
 
 void loop() {
-  if (ifButtonPressed()) {
+  bool buttonDown = digitalRead(buttonPin) == LOW;
 
-    camera_fb_t* fb = takePhoto();
-    if (fb == NULL) return;
-
-    uint8_t* resized = resizeImage(fb);
-    uint8_t* dithered = applyDithering(resized, 384, 288);
-    uint8_t* bitmap = convertImage(dithered, 384, 288);
-
-    printImage(bitmap, 384, 288);
-
-    esp_camera_fb_return(fb);
-    delete[] resized;
-    delete[] bitmap;
-
-    delay(1000);
+  if (buttonDown && !buttonWasDown) {
+    // button was just pressed
+    buttonWasDown = true;
+    buttonDownStart = millis();
+    longPressHandled = false;
   }
-}
-bool ifButtonPressed(){
-  return digitalRead(buttonPin)== LOW;
 
+  if (buttonDown && buttonWasDown) {
+    // button is being held
+    if (!longPressHandled && millis() - buttonDownStart >= HOLD_TIME) {
+      longPressHandled = true;
+      enterOffMode();
+    }
+  }
+
+  if (!buttonDown && buttonWasDown) {
+    // button was released
+    unsigned long pressLength = millis() - buttonDownStart;
+    buttonWasDown = false;
+
+    if (!longPressHandled && pressLength < HOLD_TIME) {
+      Serial.println("Short press: capture and print");
+      takePhotoAndPrint();
+    }
+
+    delay(200); // debounce
+  }
+
+  delay(20);
+}
+
+void takePhotoAndPrint() {
+  camera_fb_t* fb = takePhoto();
+  if (fb == NULL) {
+    Serial.println("Photo failed");
+    return;
+  }
+
+  uint8_t* resized = resizeImage(fb);
+  uint8_t* dithered = applyDithering(resized, 384, 288);
+  uint8_t* bitmap = convertImage(dithered, 384, 288);
+
+  printImage(bitmap, 384, 288);
+
+  esp_camera_fb_return(fb);
+
+  // dithered points to the same memory as resized,
+  // so only delete resized once.
+  delete[] resized;
+  delete[] bitmap;
+
+  delay(1000);
+}
+
+bool ifButtonPressed() {
+  return digitalRead(buttonPin) == LOW;
 }
 
 camera_fb_t* takePhoto() {
@@ -105,58 +214,53 @@ uint8_t* resizeImage(camera_fb_t *input) {
 }
 
 uint8_t* applyDithering(uint8_t *img, int width, int height) {
+  for (int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      int i = y * width + x;
 
-    for(int y=0; y<height; y++) {
+      int oldPixel = img[i];
 
-        for(int x=0; x<width; x++) {
+      int newPixel;
 
-            int i = y*width + x;
+      if (oldPixel < 128)
+        newPixel = 0;
+      else
+        newPixel = 255;
 
-            int oldPixel = img[i];
+      img[i] = newPixel;
 
-            int newPixel;
+      int error = oldPixel - newPixel;
+      int spread = error / 8;
 
-            if(oldPixel < 128)
-                newPixel = 0;
-            else
-                newPixel = 255;
+      auto addError = [&](int xx, int yy) {
+        if (xx < 0 || xx >= width || yy < 0 || yy >= height)
+          return;
 
-            img[i] = newPixel;
+        int index = yy * width + xx;
 
-            int error = oldPixel - newPixel;
-            int spread = error/8;
+        int value = img[index] + spread;
 
-            auto addError=[&](int xx,int yy){
+        if (value < 0)
+          value = 0;
 
-                if(xx<0 || xx>=width ||
-                   yy<0 || yy>=height)
-                    return;
+        if (value > 255)
+          value = 255;
 
-                int index = yy*width + xx;
+        img[index] = value;
+      };
 
-                int value = img[index] + spread;
+      addError(x + 1, y);
+      addError(x + 2, y);
 
-                if(value<0)
-                    value=0;
+      addError(x - 1, y + 1);
+      addError(x, y + 1);
+      addError(x + 1, y + 1);
 
-                if(value>255)
-                    value=255;
-
-                img[index]=value;
-            };
-
-            addError(x+1,y);
-            addError(x+2,y);
-
-            addError(x-1,y+1);
-            addError(x,y+1);
-            addError(x+1,y+1);
-
-            addError(x,y+2);
-        }
+      addError(x, y + 2);
     }
+  }
 
-    return img;
+  return img;
 }
 
 uint8_t* convertImage(uint8_t* image, int width, int height) {
@@ -167,7 +271,6 @@ uint8_t* convertImage(uint8_t* image, int width, int height) {
 
   for (int y = 0; y < height; y++) {
     for (int byteX = 0; byteX < bytesPerRow; byteX++) {
-
       uint8_t b = 0;
 
       for (int bit = 0; bit < 8; bit++) {
@@ -211,4 +314,3 @@ void printImage(uint8_t* bitmap, int width, int height) {
 
   printerSerial.write(0x0A);
 }
-
